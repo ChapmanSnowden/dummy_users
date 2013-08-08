0355d66f43861a373b8dd696bb732cc62f8ab453
Apprentice <apprentice@dbc15.local>
2013-08-07 18:53:25 -0700

    includes new viewpages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,23 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post 'login' do
+
+  erb :user_landing
+end
+
+get 'create_account' do
+  erb :new_user
+end
+
+
+post 'create_account' do
+
+  redirect to '/'
+end
+
+post 'logout' do
+
+  erb :index
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,31 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+  # e.g., User.authenticate('jesse@devbootcamp.com', 'apples123')
+  
+
+
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    # if email and password correspond to a valid user, return that user
+    # otherwise, return nil
+
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      @user
+    else
+      nil
+    end
+
+  end
 end
